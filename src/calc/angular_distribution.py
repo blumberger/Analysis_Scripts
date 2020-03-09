4161921d2bdfa9c680e8d318579db1c77c8e3a03
Matt <95ellismle@gmail.com>
2020-03-09 10:10:51 +0000

    Improved angular distribution calculation

--- src/calc/angular_distribution.py
+++ src/calc/angular_distribution.py
@@ -32,12 +32,10 @@ class Angular_Dist(gen_type.Calc_Type):
     _write_types = ('json', )
     required_metadata = ('atoms_per_molecule',)
     _defaults = {'number_bins': 'auto', 'histogram_density': True,
-                 'short_axis_atoms': [[23, 24], [22, 25],
-                                      [21, 26], [20, 27],
-                                      [19, 18], [34, 10],
-                                      [9, 0], [8, 1], [7, 2],
-                                      [6, 3], [5, 4]],
-                 'long_axis_atoms': [[23, 5], [24, 4], [22, 6], [25, 3]],
+                 'short_axis_atoms': [[5, 6], [4, 7], [3, 8], [1, 28], [0, 18],
+                                      [10, 19], [27, 20], [26, 21], [25, 22],
+                                      [24, 23]],
+                 'long_axis_atoms': [[24, 5], [23, 6]],
                 }
     # Need these 3 attributes to create a new variable type
     data = {}
@@ -62,44 +60,83 @@ class Angular_Dist(gen_type.Calc_Type):
             short_ax_ats = self.metadata['short_axis_atoms']
         else: short_ax_ats = self.Var.metadata['short_axis_atoms']
 
+        # Divide atomic coordinates into molecular coordinates
         all_at_crds = self.compute_data
+        all_mol_crds = mol_utils.atoms_to_mols(all_at_crds, ats_per_mol,
+                                               nstep=len(all_at_crds))
 
-        self.long_ax_angles, self.long_ax_vecs = [], []
-        self.long_ax_bin_edges, self.long_ax_counts = [], []
-        self.short_ax_angles, self.short_ax_vecs = [], []
-        self.short_ax_bin_edges, self.short_ax_counts = [], []
-        for at_crds in all_at_crds:
-            # Divide atomic coordinates into molecular coordinates
-            mol_crds = mol_utils.atoms_to_mols(at_crds, ats_per_mol,
-                                                    nstep=len(at_crds))
+        # Loop over all steps
+        for mol_crds in all_mol_crds:
 
-            # Get the long ax angles
-            _ = self.get_angle_dist(mol_crds, long_ax_ats)
-            self.long_ax_angles.append(_[0])
-            self.long_ax_vecs.append(_[1])
-            hist = np.histogram(_[0], bins=self.metadata['number_bins'],
-                               density=self.metadata['histogram_density'])
-            self.long_ax_counts.append(hist[0])
-            self.long_ax_bin_edges.append(hist[1])
+            # Get the vectors that describe the axes
+            self.long_vecs = self.get_all_ax_vecs(long_ax_ats, mol_crds)
+            self.short_vecs = self.get_all_ax_vecs(short_ax_ats, mol_crds)
+            long_mags = np.linalg.norm(self.long_vecs, axis=1)
+            short_mags = np.linalg.norm(self.short_vecs, axis=1)
 
-            # Get the long ax angles
-            _ = self.get_angle_dist(mol_crds, short_ax_ats)
-            self.short_ax_angles.append(_[0])
-            self.short_ax_vecs.append(_[1])
-            hist = np.histogram(_[0], bins=self.metadata['number_bins'],
-                               density=self.metadata['histogram_density'])
-            self.short_ax_counts.append(hist[0])
-            self.short_ax_bin_edges.append(hist[1])
+            # Get the angle of the long and short axis with every other molecule
+            nmol = len(mol_crds)
+            data_size = int(nmol * (nmol - 1) / 2)
+            self.long_angles = np.zeros(data_size)
+            self.short_angles = np.zeros(data_size)
+            count = 0
+            for mol1 in range(1, nmol):
+                long_angles = self.get_angles_between_vecs(mol1, self.long_vecs,
+                                                           long_mags)
+                short_angles = self.get_angles_between_vecs(mol1, self.short_vecs,
+                                                            short_mags)
 
-        self.short_ax_angles = np.array(self.short_ax_angles)
-        self.short_ax_vecs = np.array(self.short_ax_vecs)
-        self.short_ax_bin_edges = np.array(self.short_ax_bin_edges)
-        self.short_ax_counts = np.array(self.short_ax_counts)
+                self.short_angles[count: count + len(short_angles)] = short_angles
+                self.long_angles[count: count + len(short_angles)] = long_angles
+                count += len(short_angles)
 
-        self.long_ax_angles = np.array(self.long_ax_angles)
-        self.long_ax_vecs = np.array(self.long_ax_vecs)
-        self.long_ax_bin_edges = np.array(self.long_ax_bin_edges)
-        self.long_ax_counts = np.array(self.long_ax_counts)
+            # Convert to degress cuz they're nicer
+            #self.long_angles  *= 180. / np.pi
+            #self.short_angles *= 180. / np.pi
+
+            self.long_counts, self.long_bin_edges = np.histogram(self.long_angles,
+                                  density=True, bins=self.metadata['number_bins'])
+            self.short_counts, self.short_bin_edges = np.histogram(self.short_angles,
+                                  density=True, bins=self.metadata['number_bins'])
+
+    def get_angles_between_vecs(self, vec_ind, vecs, mags):
+        """
+        Will get the angle a vector makes with many other vectors
+
+        Inputs:
+            * vec_ind <int> => The index of the vector to compare with
+            * vecs <array> => All vectors
+            * mags <array> => The magnitude of all the vectors
+        Outputs:
+            <array> All angles between vecA and other_vecs
+        """
+        vecA = vecs[vec_ind]
+        magA = mags[vec_ind]
+        vecs = vecs[:vec_ind]
+        mags = mags[:vec_ind]
+
+        dots = np.sum(vecA * vecs, axis=1)
+        mags = magA * mags
+        angles = dots/mags# * 180/np.pi
+
+        return angles
+
+    def get_all_ax_vecs(self, at_inds, mol_crds):
+        """
+        Will get the vector that the atoms with ID long_ac_ats of each mol form.
+
+        Inputs:
+            * at_inds <list<int>> => the indices of the atoms on the axis you want
+            * mol_crds <list<float>> => The molecular coords in shape (nmol, nat per mol, 3)
+        """
+        if len(np.shape(at_inds)) == 1:
+            at_inds = [at_inds]
+
+        ats = mol_crds[:, at_inds, :]
+        ats = np.mean(ats, axis=1)
+        vecs = ats[:, 0] - ats[:, 1]
+        return vecs
+
 
     def json_data(self):
         """
@@ -108,17 +145,17 @@ class Angular_Dist(gen_type.Calc_Type):
         This function is just used for writing
         """
         data = {
-                'short_ax_angles': self.short_ax_angles.tolist(),
-                'short_ax_vecs': self.short_ax_vecs.tolist(),
+                'short_ax_angles': self.short_angles.tolist(),
+                'short_ax_vecs': self.short_vecs.tolist(),
                 'short_ax_histogram': {
-                                        'counts': self.short_ax_counts.tolist(),
-                                        'bin_edges': self.short_ax_bin_edges.tolist(),
+                                        'counts': self.short_counts.tolist(),
+                                        'bin_edges': self.short_bin_edges.tolist(),
                                       },
-                'long_ax_angles': self.long_ax_angles.tolist(),
-                'long_ax_vecs': self.long_ax_vecs.tolist(),
+                'long_ax_angles': self.long_angles.tolist(),
+                'long_ax_vecs': self.long_vecs.tolist(),
                 'long_ax_histogram':  {
-                                        'counts': self.long_ax_counts.tolist(),
-                                        'bin_edges': self.long_ax_bin_edges.tolist(),
+                                        'counts': self.long_counts.tolist(),
+                                        'bin_edges': self.long_bin_edges.tolist(),
                                       },
                 }
         return data
@@ -169,11 +206,14 @@ class Angular_Dist(gen_type.Calc_Type):
             (plt.bar, plt.axis) bar class and axis that the hist has been plotted on.
         """
         # Convert to degress
-        edges = bin_edges * 180 / np.pi
-
-        bars = axis.bar(edges[:-1], counts, width=np.diff(edges), label=label,
-                        alpha=0.7)
-        return bars, axis
+        #bin_edges = bin_edges
+        #bars = axis.bar(bin_edges[:-1], counts, width=np.diff(bin_edges),
+        #                label=label, alpha=0.7)
+        min_bin, max_bin = min(bin_edges), max(bin_edges)
+        axis.plot([-1, min_bin], [0, 0], 'k-')
+        axis.plot([max_bin, 1], [0, 0], 'k-')
+        line, = axis.plot(bin_edges[:-1], counts, 'k-')
+        return line, axis
 
     def plot(self, axes=False, label=""):
         """
@@ -191,26 +231,27 @@ class Angular_Dist(gen_type.Calc_Type):
 
         # Plot long axis angle distribution
         all_bars = []
-        bars, ax = self.plot_single(axes[0], self.long_ax_bin_edges[0],
-                                    self.long_ax_counts[0], label=label)
+        bars, ax = self.plot_single(axes[0], self.long_bin_edges,
+                                    self.long_counts, label=label)
         all_bars.append(bars)
         axes[0] = ax
 
         # Plot short axis angle distribution
         all_bars = []
-        bars, ax = self.plot_single(axes[1], self.short_ax_bin_edges[0],
-                                    self.short_ax_counts[0], label=label)
+        bars, ax = self.plot_single(axes[1], self.short_bin_edges,
+                                    self.short_counts, label=label)
         all_bars.append(bars)
         axes[1] = ax
 
         # Make it pretty
-        axes[0].set_ylabel("Long Ax Density", fontsize=16)
-        axes[1].set_ylabel("Short Ax Density", fontsize=16)
-        axes[1].set_xlabel(r"Angle [$^o$]", fontsize=16)
+        axes[0].set_ylabel("Long Ax Density", fontsize=24)
+        axes[1].set_ylabel("Short Ax Density", fontsize=26)
+        axes[1].set_xlabel(r"cos($\theta$)", fontsize=26)
 
-        axes[0].legend(fontsize=16)
+        if label:
+            axes[0].legend(fontsize=16)
         for i, ax in enumerate(axes):
-            ax.set_xlim([0, 180])
+            ax.set_xlim([-1, 1])
         plt.tight_layout()
 
         return axes

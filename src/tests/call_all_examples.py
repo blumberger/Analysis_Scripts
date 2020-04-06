25a02a9187c9b653bc2175e8e82bdc2768dc0822
Matt <95ellismle@gmail.com>
2020-04-06 14:23:55 +0100

    Finally tidied up the root folder and added another example (for calculating the layers).
    Also added some possibly useful scripts into scripts/old_input_files.

--- src/tests/call_all_examples.py
+++ src/tests/call_all_examples.py
@@ -126,8 +126,12 @@ all_example_tests = {
                                         'test_func': check_stdout,
                                         'inp_file' : 'calc_couplings.inp',
                                         'out_file' : [f"Hab_connect_layer_{i}.png" for i in range(0, 8)],
-                                                     },
-
+                                                     },     
+                        'calc_layers': {
+                                        'test_func': check_stdout,
+                                        'inp_file' : 'get_layers.inp',
+                                        'out_file' : False,
+                                       },
 
 
                       # Keep this one last
